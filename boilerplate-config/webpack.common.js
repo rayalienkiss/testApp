0539dce10df0b86ee1085e991377114eba45f7ad
rayalienkiss <alien1983gs@126.com>
2016-12-20 18:26:07 +0800

    update

--- boilerplate-config/webpack.common.js
+++ boilerplate-config/webpack.common.js
@@ -1,16 +1,31 @@
-var Webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
+
+/*
+// 目录
+// --------------------------------------------------
+*/
+// 项目根目录
+const ROOT_PATH = path.resolve(__dirname, '..');
+// 源码目录
+const SRC_PATH = path.join(ROOT_PATH, 'src');
+
 module.exports = {
     entry: ["../src/entries/app.js"],
     output: {
-        path: __dirname,
+        path: path.join(ROOT_PATH, 'dist'),
         filename: "bundle.js"
     },
     module: {
         loaders: [
-            {
-                test: /\.css$/,
-                loader: "style!css"
-            }
+            // {
+            //     test: /\.css$/,
+            //     loader: "style!css"
+            // },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.js$/, loader: 'jsx-loader?harmony' },
+            { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
         ]
     }
 };
